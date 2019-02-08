f960363f267b7f2652cd957e25c0914a3044fb60
Eagan Martin <mece.martinece@gmail.com>
2019-02-08 19:49:15 +0800

    Added endpoint to server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express'),
 	app = express(),
-	port = process.env.PORT || 3000;
+	port = process.env.PORT || 3000
+	bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var arithmeticEndpoint = require('./api/endpoints/arithmetic-endpoint');
+arithmeticEndpoint(app);
 
 app.listen(port);
 
