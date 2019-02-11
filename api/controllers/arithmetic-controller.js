5b5f4808790f86f74b443e1525f54dee0b1bae1d
Eagan Martin <mece.martinece@gmail.com>
2019-02-11 13:20:42 +0800

    Revised add function

--- api/controllers/arithmetic-controller.js
+++ api/controllers/arithmetic-controller.js
@@ -1,3 +1,16 @@
+var mongoose = require('mongoose'),
+	Computation = mongoose.model('Computations');
+
+var saveComputation = (result) => {
+	let newComputation = new Computation(result)
+
+	newComputation
+		.save((error, computation) => {
+			if(error) response.send(error);
+			response.json(computation);
+		});
+};
+
 exports.index = (request, response) => {
 	response.json({
 		code: 'success',
@@ -9,12 +22,20 @@ exports.add = (request, response) => {
 	let body = request.body,
 		a = body.a,
 		b = body.b,
-		result = a + b;
+		computed = a + b;
 
-	response.json({
-		code: 'success',
-		result: result
-	});
+	const result = {
+		requested_by: request.ip,
+		endpoint: request.route.path,
+		operands: {
+			a: a,
+			b: b
+		},
+		message: 'success',
+		result: computed
+	};
+
+	response.json(result);
 };
 
 exports.subtract = (request, response) => {
