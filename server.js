b80f7965cc67bec6305c24fead18a6e9669a9811
Eagan Martin <mece.martinece@gmail.com>
2019-02-11 13:16:11 +0800

    Added mongodb connection on server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express'),
 	app = express(),
-	port = process.env.PORT || 3000
+	port = process.env.PORT || 3000,
+	mongoose = require('mongoose'),
 	bodyParser = require('body-parser'),
+	Computation = require('./api/models/response-model'),
 	apiController = require('./api/controllers/api-controller'),
 	errorController = require('./api/controllers/error-controller');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/computedb', { useNewUrlParser: true }); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
