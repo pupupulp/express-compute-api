eb8c10a9ce0310798fdeaa2f51e4616cddd5a5de
Eagan Martin <mece.martinece@gmail.com>
2019-02-11 14:18:19 +0800

    Added physics on routes

--- api/controllers/api-controller.js
+++ api/controllers/api-controller.js
@@ -1,4 +1,7 @@
 module.exports = (app) => {
-	var arithmeticEndpoint = require('../endpoints/arithmetic-endpoint');
+	var arithmeticEndpoint = require('../endpoints/arithmetic-endpoint'),
+		physicsEndpoint = require('../endpoints/physics-endpoint');
+	
 	arithmeticEndpoint(app);
+	physicsEndpoint(app);
 }
