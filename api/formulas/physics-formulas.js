626893ca684c8289c0e72271fd8b2c7c153155dd
Eagan Martin <mece.martinece@gmail.com>
2019-02-11 15:02:38 +0800

    Added functions for physics formulas

--- api/formulas/physics-formulas.js
+++ api/formulas/physics-formulas.js
@@ -1,8 +1,72 @@
 exports.kinematicalMotion = (operands) => {
-	let s = (operands.u * operands.t) + (1 / 2) * (operands.a * operands.t) ^ 2,
+	let s = (operands.u * operands.t) + (1 / 2) * (operands.a * (operands.t ^ 2)),
 		v = operands.u + (operands.a * operands.t),
-		v2 = operands.u ^ 2 + (2 * operands.a * s),
+		v2 = (operands.u ^ 2) + (2 * operands.a * s),
 		vav = (v + operands.u) / 2;
 
 	return vav;
+}
+
+exports.momentum = (operands) => {
+	return operands.m * operands.v;
+}
+
+exports.force = (operands) => {
+	return operands.m * operands.a;
+}
+
+exports.impulse = (operands) => {
+	return operands.m * (operands.v - operands.u);
+}
+
+exports.pressure = (operands) => {
+	return operands.f / operands.a;
+}
+
+exports.density = (operands) => {
+	return operands.m / operands.v;
+}
+
+exports.angularMomentum = (operands) => {
+	return operands.r * operands.p;
+}
+
+exports.torque = (operands) => {
+	return operands.r * operands.f;
+}
+
+exports.centripetalForce = (operands) => {
+	return (operands.m * (operands.v ^ 2)) / operands.r;
+}
+
+exports.centripetalAcceleration = (operands) => {
+	return (operands.v ^ 2) / operands.r;
+}
+
+exports.centerMass = (operands) => {
+	let summationOfMr = 0,
+		summationOfM = 0;
+
+	for(let i = 0; i < operands.n; i++) {
+		summationOfMr += 1 * operands.m[i] * operands.r[i];
+		summationOfM += 1 * operands.m[i];
+	}
+
+	return summationOfMr / summationOfM;
+}
+
+exports.reducedMass = (operands) => {
+	return (operands.m * operands.m2)  / (operands.m + operands.m2);
+}
+
+exports.work = (operands) => {
+	return operands.f * operands.d;
+}
+
+exports.potentialEnergy = (operands) => {
+	return operands.m * operands.g * operands.h;
+}
+
+exports.kineticEnergy = (operands) => {
+	return (operands.m * (1 / 2)) * (operands.v ^ 2);
 }
