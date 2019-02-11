a8c43e777ea36d11ba328914fd285d62344c5951
Eagan Martin <mece.martinece@gmail.com>
2019-02-11 15:02:19 +0800

    Added functions for physics endpoint

--- api/endpoints/physics-endpoint.js
+++ api/endpoints/physics-endpoint.js
@@ -7,4 +7,46 @@ module.exports = (app) => {
 
 	app.route(BASE_URL + '/kinematical-motion')
 		.post(controller.kinematicalMotion);
+
+	app.route(BASE_URL + '/momentum')
+		.post(controller.momentum);
+
+	app.route(BASE_URL + '/force')
+		.post(controller.force);
+
+	app.route(BASE_URL + '/impulse')
+		.post(controller.impulse);
+
+	app.route(BASE_URL + '/pressure')
+		.post(controller.pressure);
+
+	app.route(BASE_URL + '/density')
+		.post(controller.density);
+
+	app.route(BASE_URL + '/angular-momentum')
+		.post(controller.angularMomentum);
+
+	app.route(BASE_URL + '/torque')
+		.post(controller.torque);
+
+	app.route(BASE_URL + '/centripetal-force')
+		.post(controller.centripetalForce);
+
+	app.route(BASE_URL + '/centripetal-acceleration')
+		.post(controller.centripetalAcceleration);
+
+	app.route(BASE_URL + '/center-mass')
+		.post(controller.centerMass);
+
+	app.route(BASE_URL + '/reduced-mass')
+		.post(controller.reducedMass);
+
+	app.route(BASE_URL + '/work')
+		.post(controller.work);
+
+	app.route(BASE_URL + '/potential-energy')
+		.post(controller.potentialEnergy);
+
+	app.route(BASE_URL + '/kinetic-energy')
+		.post(controller.kineticEnergy);
 }
